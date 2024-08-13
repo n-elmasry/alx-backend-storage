acdd5926cab27830b249652f3753ecee2fb3551c
n-elmasry <almassrynada411@gmail.com>
2024-08-13 21:15:28 +0500

    task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """schools_by_topic"""
+from pymongo import mongo_client
 
 
 def schools_by_topic(mongo_collection, topic):
     """ returns the list of school having a specific topic"""
+    return list(mongo_collection.find({"topics": topic}))
