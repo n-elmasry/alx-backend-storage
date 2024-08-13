4b7d928f04fb5fbe89112359ff23880a0cbef9bd
n-elmasry <almassrynada411@gmail.com>
2024-08-13 22:47:23 +0500

    task 101

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
-"""top_students"""
-import pymongo
+"""
+students
+"""
 
 
 def top_students(mongo_collection):
-    """returns all students sorted by average score"""
-    students = mongo_collection.find().sort("averageScore", pymongo.DESCENDING)
-    return list(students)
+    """ Returns all students sorted by average score """
+    return mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": 1,  # Include the student's name
+                "averageScore": {  # Calculate average score
+                    "$avg": "$topics.score"
+                }
+            }
+        },
+        {
+            "$sort": {  # Sort by average score in descending order
+                "averageScore": -1
+            }
+        }
+    ])
